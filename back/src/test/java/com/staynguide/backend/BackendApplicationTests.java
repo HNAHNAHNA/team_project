a48d8c35dbbfb9198d5acad0e15e8c5c7c0adf92
MyNameIsHAN <han96305779@gmail.com>
2025-07-01 10:04:09 +0900

    nen

--- back/src/test/java/com/staynguide/backend/BackendApplicationTests.java
+++ back/src/test/java/com/staynguide/backend/BackendApplicationTests.java
@@ -1,13 +1,16 @@
 package com.staynguide.backend;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void simpleTest() {
+        assertTrue(true);
+    }
 }

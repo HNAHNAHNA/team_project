ca907657874e19f3de3ccecf3a1bad131359090a
MyNameIsHAN <han96305779@gmail.com>
2025-07-03 15:04:01 +0900

    deploy test

--- back/src/main/java/com/staynguide/backend/mapapi/controller/PlaceController.java
+++ back/src/main/java/com/staynguide/backend/mapapi/controller/PlaceController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/places")
-@CrossOrigin(origins = "localhost:5173")
+@CrossOrigin(origins = "15.164.229.102")
 public class PlaceController{
 
     private final PlaceRepository placeRepository;

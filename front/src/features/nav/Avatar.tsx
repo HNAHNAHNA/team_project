880929015c35e17f48b2aa864df31f93f885b1a2
MyNameIsHAN <ttyy9630@gmail.com>
2025-10-21 09:18:23 +0900

    ADD : new avatar image

--- front/src/features/nav/Avatar.tsx
+++ front/src/features/nav/Avatar.tsx
@@ -1,7 +1,9 @@
+import star from '../../images/star.png'
+
 function Avatar () {
     return (
         <img
-            src="https://i.namu.wiki/i/MFaPvvK3NyWEjjzEvm0sZXSjCZPrwXtCt2azFVwg4CPiMiw7n8qkcGAhWeHhkFvShg6DB3x-3nGUNorl8VYydJhVkWR5OM8VmcJStAaZ3T48fWwrxrDq_HeX1I5iNRVziZAB5PY1hQX1FmE7nIyJcw.webp"
+            src={star}
             className="
                 rounded-full
                 w-10
